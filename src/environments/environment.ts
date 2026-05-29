e452462ec92c46791720e457b96fa714a987ecfe
Nilton <nilttonalvez@gmail.com>
2026-05-29 18:36:34 -0300

    refactor: remover port of apiurl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://e-commerce-production-48f9.up.railway.app:8080',
+  apiUrl: 'https://e-commerce-production-48f9.up.railway.app',
   stripePublishableKey: 'pk_test_51TIbe0Lc6OhXmzSJc7nTWaJvCHZSEZ8oE2ITXbK1URDc9GdkSe4yTnKW26552uh8dgGXGwGRbA9fTrGv91UjVkHS00mf2bEs9a'
 };
