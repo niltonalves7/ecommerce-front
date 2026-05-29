f876830287707695963a9adc8783b23e1b86e8f1
Nilton <nilttonalvez@gmail.com>
2026-05-29 18:05:55 -0300

    feat: implementing component for user login

---
+++ src/app/features/auth/login/login.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+
+import { AuthService } from '../../../core/services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
+  templateUrl: './login.component.html'
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+  errorMessage: string | null = null;
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      this.authService.login(this.loginForm.value).subscribe({
+        next: () => {
+          // Após o login, redireciona para a vitrine de produtos
+          this.router.navigate(['/products']);
+        },
+        error: (err) => {
+          this.errorMessage = 'Usuário ou senha inválidos.';
+          console.error(err);
+          alert(this.errorMessage)
+        }
+      });
+    }
+  }
+}
