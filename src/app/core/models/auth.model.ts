b7c0f9ec47bd5bf7294481af6d50e8c3088d5e07
Nilton <nilttonalvez@gmail.com>
2026-05-29 18:14:32 -0300

    feat: implementing auth model

---
+++ src/app/core/models/auth.model.ts
@@ -0,0 +1,18 @@
+export interface LoginRequest {
+    email: string;
+    password: string;
+}
+
+export interface RegisterRequest {
+    name: string;
+    email: string;
+    password: string;
+}
+
+export interface AuthResponse {
+    token: string;
+    type: string;
+    name: string;
+    email: string;
+    role: string;
+}
