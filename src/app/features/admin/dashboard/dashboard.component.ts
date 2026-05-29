ae1d6c9aa067b0776d4104b087512d309491baf7
Nilton <nilttonalvez@gmail.com>
2026-05-29 18:09:40 -0300

    feat: implementing dashboard component

---
+++ src/app/features/admin/dashboard/dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { AuthService } from '../../../core/services/auth.service';
+
+@Component({
+    selector: 'app-dashboard',
+    standalone: true,
+    imports: [CommonModule, RouterModule],
+    templateUrl: './dashboard.component.html'
+})
+export class DashboardComponent {
+    constructor(public authService: AuthService) {}
+}
