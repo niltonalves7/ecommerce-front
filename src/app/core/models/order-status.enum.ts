045c8b5b6e9a2fac548865d5c02cf4c5f0368c1e
Nilton <nilttonalvez@gmail.com>
2026-05-29 18:13:54 -0300

    feat: implementing enum order status

---
+++ src/app/core/models/order-status.enum.ts
@@ -0,0 +1,8 @@
+export enum OrderStatus {
+    PENDING = 'PENDING',
+    PROCESSING = 'PROCESSING',
+    SHIPPED = 'SHIPPED',
+    DELIVERED = 'DELIVERED',
+    CANCELED = 'CANCELED',
+    REFUNDED = 'REFUNDED'
+}
