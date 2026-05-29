427c43d4bc182da8a1db7cc13a718344a5f29256
Nilton <nilttonalvez@gmail.com>
2026-05-29 18:18:46 -0300

    chore: add providers

---
+++ src/app/app.config.ts
@@ -0,0 +1,14 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { apiInterceptor } from './core/interceptors/api.interceptors';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([apiInterceptor]))
+  ]
+};
