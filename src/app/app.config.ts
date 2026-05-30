e48bbd3ef02cebec1c00dc4f9a8e50de7809c072
Nilton <nilttonalvez@gmail.com>
2026-05-30 02:46:08 -0300

    chore: add config of locallity for resolve error NG0701

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,18 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { apiInterceptor } from './core/interceptors/api.interceptors';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+import { LOCALE_ID } from '@angular/core';
+
+registerLocaleData(localePt);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([apiInterceptor]))
+    provideHttpClient(withInterceptors([apiInterceptor])),
+    provideBrowserGlobalErrorListeners(),
+    { provide: LOCALE_ID, useValue: 'pt-BR'} 
   ]
 };
